b7e8b80993bddb5798a1f486915f7fbcd3b1a93e
Robert Toribio <roberttoribio04@gmail.com>
2022-10-25 13:38:26 -0700

    Add a ViewHolder Implementation to PhotoGalleryFragment.kt

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,10 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import api.FlickrApi
@@ -60,6 +60,10 @@ class PhotoGalleryFragment: Fragment()
             }
         )
     }
+    private class PhotoHolder (itemTextView : TextView)
+        :RecyclerView.ViewHolder(itemTextView){
+            val bindTitle : (CharSequence) -> Unit = itemTextView::setText
+        }
 }
 
 
