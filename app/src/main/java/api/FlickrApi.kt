571066e4095dd358b31e2d7e4445769cf0314fae
Robert Toribio <roberttoribio04@gmail.com>
2022-10-26 16:12:19 -0700

    Update FlickrApi.kt

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface  FlickrApi{
 
@@ -13,4 +15,6 @@ interface  FlickrApi{
                 "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url:String) :Call <ResponseBody>
 }
