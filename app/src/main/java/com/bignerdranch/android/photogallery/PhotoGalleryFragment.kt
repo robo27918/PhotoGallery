3e7f24f6d80ddbe71b49eebbae3fb62ad1985e4a
Robert Toribio <roberttoribio04@gmail.com>
2022-10-26 15:55:11 -0700

    Update PhotoHolder from textView to ImageView

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,11 +1,13 @@
 package com.bignerdranch.android.photogallery
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -60,9 +62,9 @@ class PhotoGalleryFragment: Fragment()
             })
 
     }
-    private class PhotoHolder (itemTextView : TextView)
-        :RecyclerView.ViewHolder(itemTextView){
-            val bindTitle : (CharSequence) -> Unit = itemTextView::setText
+    private class PhotoHolder (itemImageView : ImageView)
+        :RecyclerView.ViewHolder(itemImageView){
+            val bindDrawable : (Drawable) -> Unit = itemImageView::setImageDrawable
         }
 
     private class PhotoAdapater (private val galleryItems: List <GalleryItem>)
