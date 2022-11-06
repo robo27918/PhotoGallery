8fd90bba207b24b651d6fd46a8a69bc5601c636e
Robert Toribio <roberttoribio04@gmail.com>
2022-11-06 00:14:11 -0700

    Chapter 26 Finish implementing 26.16    Prepopulating SearchView

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -105,6 +105,9 @@ class PhotoGalleryFragment: Fragment()
                     return false
                 }
             })
+            setOnSearchClickListener{
+                searchView.setQuery(photoGalleryViewModel.searchTerm,false)
+            }
         }
     }
 
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -7,10 +7,18 @@ class PhotoGalleryViewModel(private val app: Application): AndroidViewModel(app)
     val galleryItemLiveData : LiveData< List<GalleryItem> >
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
+
+    val searchTerm:String
+        get() = mutableSearchTerm.value ?: ""
     init{
         mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
-        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm){searhTerm ->
-            flickrFetchr.searchPhotos(searhTerm)
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm){ searchTerm ->
+
+            if (searchTerm.isBlank()){
+                flickrFetchr.fetchPhotos()
+            }else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
         }
     }
     fun fetchPhotos(query:String = ""){
