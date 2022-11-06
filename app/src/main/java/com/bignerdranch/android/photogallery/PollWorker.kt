c3ef7ef1ba419aae5f49dea9c042e34b88f7a010
Robert Toribio <roberttoribio04@gmail.com>
2022-11-06 12:59:50 -0800

    Implement 27.7    Fetching recent photos

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -10,7 +10,22 @@ class PollWorker (val context : Context, workerParams: WorkerParameters)
     : Worker(context, workerParams)
 {
     override fun doWork(): Result {
-        Log.i(TAG,"Work Request triggered")
+        val query = QueryPreferences.getStoredQuery(context)
+        val lastResultId = QueryPreferences.getLastResultId(context)
+        val items: List<GalleryItem> = if (query.isEmpty()){
+            FlickrFetchr().fetchPhotosRequest()
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        }else{
+
+            FlickrFetchr().searchPhotosRequest(query)
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        }?: emptyList()
         return Result.success()
     }
 }
