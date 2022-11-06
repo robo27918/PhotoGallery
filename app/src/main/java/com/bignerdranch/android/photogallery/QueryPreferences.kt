84c2fc2383e19b5db700f1fdec507c25974b9163
Robert Toribio <roberttoribio04@gmail.com>
2022-11-05 21:12:29 -0700

    Chapter 26 Finish reimplementing 26.11

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -1,4 +1,20 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.Context
+import android.preference.PreferenceManager
+import retrofit2.http.Query
+
+private const val PREF_SEARCH_QUERY = "searchQuert"
 class QueryPreferences {
+    fun getStoredQuery (context: Context): String {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getString(PREF_SEARCH_QUERY, "")!!
+    }
+
+    fun setStoredQuery (context:Context, query: String){
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .edit()
+            .putString(PREF_SEARCH_QUERY,query)
+            .apply()
+    }
 }
