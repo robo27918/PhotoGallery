611e3053b59639e05ce0d749fd47802612de2057
Robert Toribio <roberttoribio04@gmail.com>
2022-11-25 19:41:25 -0800

    Implement 28.1    Sending a broadcast intent

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -17,10 +17,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import api.FlickrApi
 import api.FlickrResponse
 import retrofit2.Call
@@ -28,9 +25,11 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 import kotlin.reflect.KProperty
 
 private const val TAG = "PhotoGalleryFragment"
+private const val POLL_WORK="POLL_WORK"
 
 class PhotoGalleryFragment: Fragment()
 {
@@ -140,6 +139,27 @@ class PhotoGalleryFragment: Fragment()
             R.id.menu_item_clear->{
                 photoGalleryViewModel.fetchPhotos("")
                 true
+            }
+            R.id.menu_item_toggle_polling -> {
+                val isPolling = QueryPreferences.isPolling(requireContext())
+                if (isPolling){
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(),false)
+                }
+                else{
+                    val constraints = Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.UNMETERED)
+                        .build()
+                    val periodicRequest=PeriodicWorkRequest
+                        .Builder(PollWorker::class.java, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(POLL_WORK,
+                    ExistingPeriodicWorkPolicy.KEEP,periodicRequest)
+                    QueryPreferences.setPolling(requireContext(),true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
             }
             else ->super.onOptionsItemSelected(item)
         }
--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -56,7 +57,12 @@ class PollWorker (val context : Context, workerParams: WorkerParameters)
                 .build()
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0,notification)
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION))
         }
         return Result.success()
+    }
+    companion object{
+        const val ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
     }
 }
