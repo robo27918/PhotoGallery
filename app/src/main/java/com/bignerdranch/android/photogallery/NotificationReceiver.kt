6819196d99f9908800a27ba7107485dcd3150c92
Robert Toribio <roberttoribio04@gmail.com>
2022-11-25 19:46:34 -0800

    Implement 28.2   First broadcast reciever

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "NotificationReceiver"
+class NotificationReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent) {
+        Log.i(TAG,"received broadcast: ${ intent.action}")
+    }
+}
