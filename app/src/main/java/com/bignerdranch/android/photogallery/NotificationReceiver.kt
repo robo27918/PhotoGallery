31016c99a3e88d800601f10bdd45454719a10245
Robert Toribio <roberttoribio04@gmail.com>
2022-11-26 13:02:00 -0800

    Implement 28.12   Implementing your result receiver

--- app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
@@ -1,13 +1,26 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Activity
+import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 private const val TAG = "NotificationReceiver"
 class NotificationReceiver : BroadcastReceiver(){
-    override fun onReceive(context: Context?, intent: Intent) {
-        Log.i(TAG,"received broadcast: ${ intent.action}")
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG,"received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK){
+            //A foreground activity cancelled the broadcast
+            return
+        }
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE,0)
+        val notification: Notification ?=
+            intent.getParcelableExtra(PollWorker.NOTIFICATION)
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (notification != null){
+            notificationManager.notify(requestCode,notification)}
     }
 }
