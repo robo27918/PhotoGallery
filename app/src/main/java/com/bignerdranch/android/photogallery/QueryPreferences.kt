eccb293ce8050fd97875c3c5284c833414f90266
Robert Toribio <roberttoribio04@gmail.com>
2022-11-25 17:59:57 -0800

    Implement 27.14     Saving worker state

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
 private const val PREF_LAST_RESULT_ID = "lastResultId"
+private const val PREF_IS_POLLING = "isPolling"
 
 object QueryPreferences {
     fun getStoredQuery (context: Context): String {
@@ -29,6 +30,15 @@ object QueryPreferences {
     fun setLastResultId(context:Context, lastResultId:String){
         PreferenceManager.getDefaultSharedPreferences(context).edit{
             putString(PREF_LAST_RESULT_ID,lastResultId)
+        }
+    }
+    fun isPolling(context:Context):Boolean{
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(PREF_IS_POLLING,false)
+    }
+    fun setPolling(context: Context, isOn:Boolean){
+        PreferenceManager.getDefaultSharedPreferences(context).edit{
+            putBoolean(PREF_IS_POLLING,isOn)
         }
     }
 }
