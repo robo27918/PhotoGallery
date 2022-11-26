c4d9fbe7a769d1fa9d7da412c01a8c2b3638523c
Robert Toribio <roberttoribio04@gmail.com>
2022-11-26 13:26:18 -0800

    Implement 29.3   Binding galleryItem

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
@@ -164,9 +165,22 @@ class PhotoGalleryFragment: VisibleFragment()
             else ->super.onOptionsItemSelected(item)
         }
     }
-    private class PhotoHolder (itemImageView : ImageView)
-        :RecyclerView.ViewHolder(itemImageView){
+    private inner class PhotoHolder (private val itemImageView : ImageView)
+        :RecyclerView.ViewHolder(itemImageView),
+         View.OnClickListener{
+            private lateinit var galleryItem: GalleryItem
+            init{
+                itemView.setOnClickListener(this)
+            }
             val bindDrawable : (Drawable) -> Unit = itemImageView::setImageDrawable
+
+            fun bindGalleryItem(item:GalleryItem){
+                galleryItem = item
+            }
+            override fun onClick(view:View){
+                val intent = Intent(Intent.ACTION_VIEW,galleryItem.photoPageUri)
+                startActivity(intent)
+            }
         }
 
     private inner class PhotoAdapater (private val galleryItems: List <GalleryItem>)
@@ -187,6 +201,7 @@ class PhotoGalleryFragment: VisibleFragment()
 
         override fun onBindViewHolder (holder:PhotoHolder , position: Int){
             val galleryItem = galleryItems[position]
+            holder.bindGalleryItem(galleryItem)
             val placeholder: Drawable = ContextCompat.getDrawable(
                 requireContext(),
                 R.drawable.bill_up_close
