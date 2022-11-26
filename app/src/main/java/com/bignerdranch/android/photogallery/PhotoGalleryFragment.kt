f0deb8f3d966e08c3fada2ec3e0d1a84f1b5e474
Robert Toribio <roberttoribio04@gmail.com>
2022-11-25 20:33:05 -0800

    Implement 28.9   Making your fragment visible

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -31,7 +31,7 @@ import kotlin.reflect.KProperty
 private const val TAG = "PhotoGalleryFragment"
 private const val POLL_WORK="POLL_WORK"
 
-class PhotoGalleryFragment: Fragment()
+class PhotoGalleryFragment: VisibleFragment()
 {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
