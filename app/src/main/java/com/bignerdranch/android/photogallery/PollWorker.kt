8765e3689637be704ccbf2bad760da973368a3af
Robert Toribio <roberttoribio04@gmail.com>
2022-11-25 21:47:08 -0800

    Implement 28.11   Sending an ordered broadcast

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -55,15 +56,27 @@ class PollWorker (val context : Context, workerParams: WorkerParameters)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .build()
-            val notificationManager = NotificationManagerCompat.from(context)
-            notificationManager.notify(0,notification)
-            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION))
+            showBackgroundNotification(0,notification)
         }
         return Result.success()
     }
+    private fun showBackgroundNotification(
+        requestCode:Int,
+        notification:Notification
+    ){
+        val intent = Intent(ACTION_SHOW_NOTIFICATION).apply{
+            putExtra(REQUEST_CODE,requestCode)
+            putExtra(NOTIFICATION,notification)
+        }
+        context.sendOrderedBroadcast(intent, PERM_PRIVATE)
+    }
     companion object{
-        const val ACTION_SHOW_NOTIFICATION =
-            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
-        const val PERM_PRIVATE = "com.bignerdranch.android.photgallery.PRIVATE"
+        const val ACTION_SHOW_NOTIFICATION
+        ="com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
+
+        const val PERM_PRIVATE = "com.bignerdranch.android.photogallery.PRIVATE"
+
+        const val REQUEST_CODE = "REQUEST_CODE"
+        const val NOTIFICATION = "NOTIFICATION"
     }
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.kt
@@ -1,19 +1,20 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-
+private const val TAG = "VisibleFragment"
 abstract class VisibleFragment : Fragment(){
     private val onShowNotification = object: BroadcastReceiver(){
-        override fun onReceive(context: Context?, intent: Intent) {
-            Toast.makeText(requireContext(),
-            "Got a broadcast ${intent.action}",
-            Toast.LENGTH_LONG
-            ).show()
+        override fun onReceive(context: Context, intent: Intent) {
+            Log.i(TAG,"canceling notification")
+            resultCode = Activity.RESULT_CANCELED
+
         }
     }
     override fun onStart(){
